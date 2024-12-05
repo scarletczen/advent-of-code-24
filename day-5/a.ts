c6e33a1435fb968f18b33995d27e782e8c894f57
scarletczen <abhinavthapa1998@gmail.com>
2024-12-05 19:29:23 +0530

    feat: added day-5 solution

--- day-5/a.ts
+++ day-5/a.ts
@@ -1,9 +1,47 @@
 import { runSolution } from '../utils.ts';
 
-/** provide your solution as the return of this function */
-export async function day5a(data: string[]) {
-  console.log(data);
-  return 0;
+function ints(s, neg = true) {
+  let reg = /\d+/g;
+  if (neg) reg = /-?\d+/g;
+
+  return [...s.matchAll(reg)].map((x) => +x[0]);
 }
 
-await runSolution(day5a);
+/** provide your solution as the return of this function */
+export async function day5a(data: Buffer) {
+  const [list, inp] = data
+    ?.toString()
+    ?.trim()
+    ?.split('\n\n')
+    ?.map((x) => x?.split('\n')?.map((y) => ints(y)));
+  let [ret1, ret2] = [0, 0];
+
+  const rules = {};
+  for (const line of list) {
+    rules[[line[0], line[1]]] = true;
+  }
+
+  for (const line of inp) {
+    let changed = true;
+    let everchanged = false;
+    while (changed) {
+      changed = false;
+      for (let i = 0; i < line.length; i++) {
+        for (let j = i + 1; j < line.length; j++) {
+          if (rules[[line[j], line[i]]]) {
+            changed = true;
+            everchanged = true;
+            const tmp = line[i];
+            line[i] = line[j];
+            line[j] = tmp;
+          }
+        }
+      }
+    }
+    if (everchanged) ret2 += line[(line.length - 1) / 2];
+    else ret1 += line[(line.length - 1) / 2];
+  }
+  return ret1;
+}
+
+await runSolution(day5a, { rawFile: true });
