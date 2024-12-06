d55772007d5122c1f9a46d17da33e43670245ef7
scarletczen <abhinavthapa1998@gmail.com>
2024-12-06 20:02:45 +0530

    feat: added day-6 solution

--- day-6/a.ts
+++ day-6/a.ts
@@ -1,9 +1,168 @@
 import { runSolution } from '../utils.ts';
 
-/** provide your solution as the return of this function */
-export async function day6a(data: string[]) {
-  console.log(data);
-  return 0;
+export function removeFromArray<T extends unknown[]>(
+  arr: T,
+  idx: number,
+  count = 1
+) {
+  const copy = [...arr];
+  copy.splice(idx, count);
+  return copy;
 }
 
-await runSolution(day6a);
+export function copy2DArray<T extends unknown[][]>(arr: T) {
+  return arr.map((arr) => arr.slice()) as T;
+}
+
+const wall = '#';
+
+type Guard = '^' | '>' | 'v' | '<';
+
+const GuardDirection: Record<Guard, readonly [number, number]> = {
+  '^': [-1, 0],
+  '>': [0, 1],
+  '<': [0, -1],
+  v: [1, 0],
+};
+
+const GuardRotation: Record<Guard, Guard> = {
+  '^': '>',
+  '>': 'v',
+  '<': '^',
+  v: '<',
+};
+
+function getGuardPosition(grid: string[][]) {
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[row].length; col++) {
+      if (Object.keys(GuardDirection).includes(grid[row][col])) {
+        return [row, col] as const;
+      }
+    }
+  }
+}
+
+function isGuardInBounds(pos: readonly [number, number], gridLength: number) {
+  return (
+    pos[0] < gridLength && pos[0] >= 0 && pos[1] < gridLength && pos[1] >= 0
+  );
+}
+
+function getGuardNextPosition(
+  currentGuard: Guard,
+  [currentRow, currentCol]: readonly [number, number]
+) {
+  const [dRow, dCol] = GuardDirection[currentGuard];
+  return [currentRow + dRow, currentCol + dCol] as const;
+}
+
+function getGuardPath(
+  grid: string[][],
+  initialGuardPosition: readonly [number, number]
+) {
+  const spots = new Set<string>([]);
+
+  let guardPosition = initialGuardPosition;
+
+  if (!guardPosition) {
+    throw new Error('No guard found!');
+  }
+
+  spots.add(guardPosition.join(','));
+
+  while (true) {
+    const currentGuard = grid[guardPosition[0]][guardPosition[1]] as Guard;
+    const guardNextPosition = getGuardNextPosition(currentGuard, guardPosition);
+
+    if (!isGuardInBounds(guardNextPosition, grid.length)) {
+      break;
+    }
+
+    if (grid[guardNextPosition[0]][guardNextPosition[1]] !== wall) {
+      grid[guardNextPosition[0]][guardNextPosition[1]] = currentGuard;
+      grid[guardPosition[0]][guardPosition[1]] = 'X';
+      spots.add(guardNextPosition.join(','));
+      guardPosition = guardNextPosition;
+    } else {
+      grid[guardPosition[0]][guardPosition[1]] = GuardRotation[currentGuard];
+    }
+  }
+
+  return [...spots].map((spot) => spot.split(',').map(Number));
+}
+
+// async function solvePart2() {
+//   const originalGrid = await readGrid(__dirname, { testInput: false });
+
+//   let loopCount = 0;
+//   const initialGuardPosition = getGuardPosition(originalGrid);
+
+//   if (!initialGuardPosition) {
+//     throw new Error('No guard found!');
+//   }
+
+//   const possibleObstaclePositions = getGuardPath(
+//     copy2DArray(originalGrid),
+//     initialGuardPosition
+//   );
+//   possibleObstaclePositions.shift();
+
+//   for (const [obstascleRow, obstacleCol] of possibleObstaclePositions) {
+//     const rotations = new Set<string>();
+//     const grid = copy2DArray(originalGrid);
+//     rotations.add(
+//       grid[initialGuardPosition[0]][initialGuardPosition[1]] +
+//         initialGuardPosition.join('')
+//     );
+//     grid[obstascleRow][obstacleCol] = wall;
+
+//     let guardPosition = initialGuardPosition;
+
+//     while (true) {
+//       const currentGuard = grid[guardPosition[0]][guardPosition[1]] as Guard;
+//       const guardNextPosition = getGuardNextPosition(
+//         currentGuard,
+//         guardPosition
+//       );
+
+//       if (!isGuardInBounds(guardNextPosition, grid.length)) {
+//         break;
+//       }
+
+//       if (grid[guardNextPosition[0]][guardNextPosition[1]] !== wall) {
+//         grid[guardNextPosition[0]][guardNextPosition[1]] = currentGuard;
+//         grid[guardPosition[0]][guardPosition[1]] = '.';
+//         guardPosition = guardNextPosition;
+//       } else {
+//         const nextRotation = GuardRotation[currentGuard];
+//         grid[guardPosition[0]][guardPosition[1]] = nextRotation;
+//         const nextRotationEntry = nextRotation + guardPosition.join('');
+
+//         if (rotations.has(nextRotationEntry)) {
+//           loopCount += 1;
+//           break;
+//         } else {
+//           rotations.add(nextRotationEntry);
+//         }
+//       }
+//     }
+//   }
+
+//   logger.logSolution({ part: 2, result: loopCount });
+// }
+
+/** provide your solution as the return of this function */
+export async function day6a(data: Buffer) {
+  const inputData = data?.toString()?.split('\n');
+  const grid = inputData?.map((line) => line?.split(''));
+  const initialGuardPosition = getGuardPosition(grid);
+
+  if (!initialGuardPosition) {
+    throw new Error('No guard found!');
+  }
+
+  const spots = getGuardPath(grid, initialGuardPosition);
+  return spots?.length;
+}
+
+await runSolution(day6a, { rawFile: true });
--- day-6/b.ts
+++ day-6/b.ts
@@ -1,9 +1,106 @@
 import { runSolution } from '../utils.ts';
 
-/** provide your solution as the return of this function */
-export async function day6b(data: string[]) {
-  console.log(data);
-  return 0;
+type Coord = [number, number];
+
+const Grid = {
+  cells: [] as number[][],
+
+  curDir: 0,
+  directions: [
+    [0, -1],
+    [1, 0],
+    [0, 1],
+    [-1, 0],
+  ] as Coord[],
+
+  guardPos: [-1, -1] as Coord,
+  initialPos: [-1, -1] as Coord,
+
+  parseGrid(input: string[]) {
+    this.cells = input.map((line, y) =>
+      line.split('').map((char, x) => {
+        if (char === '^') this.initialPos = [x, y];
+        return char === '#' ? 1 : 0;
+      })
+    );
+  },
+
+  resetPosition() {
+    this.guardPos = this.initialPos;
+    this.curDir = 0;
+  },
+
+  runPatrol() {
+    this.resetPosition();
+
+    const visited = new Set<string>([this.guardPos.join(',')]);
+    let iters = 0,
+      escaped = false;
+
+    do {
+      const nextPos = [
+        this.guardPos[0] + this.directions[this.curDir][0],
+        this.guardPos[1] + this.directions[this.curDir][1],
+      ] as Coord;
+
+      const nextVal = this.cells[nextPos[1]]?.[nextPos[0]] ?? -1;
+
+      if (nextVal < 0) {
+        escaped = true;
+        break;
+      }
+
+      if (nextVal === 1) {
+        this.curDir = (this.curDir + 1) % 4;
+      } else {
+        this.guardPos = nextPos;
+
+        visited.add(this.guardPos.join(','));
+      }
+    } while (iters++ < 6e3);
+
+    return escaped ? visited : Infinity;
+  },
+};
+
+export function part1(input: string[]) {
+  Grid.parseGrid(input);
+  const result = Grid.runPatrol();
+
+  if (result instanceof Set) {
+    return result.size;
+  }
+
+  return NaN;
 }
 
-await runSolution(day6b);
+export function part2(input: string[]) {
+  Grid.parseGrid(input);
+
+  const visited = Grid.runPatrol();
+  let newObstacleCount = 0;
+
+  if (visited instanceof Set) {
+    for (const cs of visited) {
+      const coord = cs.split(',').map(Number) as Coord;
+      Grid.cells[coord[1]][coord[0]] = 1;
+
+      const result = Grid.runPatrol();
+
+      if (result === Infinity) {
+        newObstacleCount += 1;
+      }
+
+      Grid.cells[coord[1]][coord[0]] = 0;
+    }
+  }
+
+  return newObstacleCount;
+}
+
+/** provide your solution as the return of this function */
+export async function day6b(data: Buffer) {
+  return part2(data?.toString()?.split('\n'));
+}
+
+await runSolution(day6b, { rawFile: true });
